0bf45c5603dcbe0e32b86ebb6ae5ab84c7358275
Lance Wallis <bleudeveloper@gmail.com>
2020-09-28 18:32:49 -0500

    UI top level cleanup.

--- ui/app/app.module.ts
+++ ui/app/app.module.ts
@@ -1,25 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './@core/core.module';
+import { ThemeModule } from './@theme/theme.module';
+
 import { 
-  NbSidebarModule, 
-  NbLayoutModule 
+  NbSidebarModule,
+  NbMenuModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbWindowModule,
+  NbToastrModule,
 } from '@nebular/theme';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    NbLayoutModule,
-    NbSidebarModule.forRoot(),
-
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ThemeModule.forRoot(),
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbDatepickerModule.forRoot(),
+    NbDialogModule.forRoot(),
+    NbWindowModule.forRoot(),
+    NbToastrModule.forRoot(),
     AppRoutingModule,
+    CoreModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
