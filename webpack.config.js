b913b0e48fbb8f69c7b418d1ef1b0beffbab2fa6
Lance Wallis <bleudeveloper@gmail.com>
2020-12-06 17:07:41 -0600

    Structuring.

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode == 'production';
+
+    return {
+        entry: {
+            bundle: './ui/index.js',
+            style: './ui/index.scss'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'resources', 'app'),
+            publicPath: ""
+        },
+        resolve: {
+            alias: {
+                Utilities: path.resolve('ui/js/')
+            },
+            extensions: ['.js', '.json', '.jsx']
+        },
+        devtool: (isProduction) ? "none" : "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                [
+                                    '@babel/preset-react', {
+                                        development: !isProduction
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                            options: {
+                                "sourceMap": !isProduction,
+                            }
+                        },
+                        "resolve-url-loader",
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                // always make sourceMap. resolver-url-loader is needing it
+                                "sourceMap": true,
+                            }
+                        }
+                    ]
+                },
+                
+            ]
+        }
+    }
+}
